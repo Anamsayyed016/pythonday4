ef174c44cf25c4bbfadc72cb2604bb36adf38bcb
anamsayyed016 <anamsayyed58@gmail.com>
2025-01-22 14:13:06 +0530

    22jan

---
+++ datatype.py
@@ -0,0 +1,97 @@
+# datatype
+# numeric- integer, float, complex
+
+# string              
+# list
+# tuple
+# dict--- maped data type
+# set
+# frozenset
+# boolean
+
+# collection
+# ordered ------ string, list, tuple, dict
+# unordered-------set, frozenset---its hold unqiue content-- can not use duplicate content.
+# ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+# Numeric-
+# Integer:-
+# x=10
+# y=20
+# print(type(x))
+# print(type(y))
+# z=x+y
+# print(type(z))
+# output-<class 'int'>
+# <class 'int'>
+# <class 'int'>
+
+# x=10
+# y=20
+# print(type(x))
+# print(type(y))
+# z=x-y
+# print(type(z))
+
+# x=10
+# y=20
+# print(type(x))
+# print(type(y))
+# z=x*y
+# print(type(z))
+# print(type(z))
+
+# x=10
+# y=20
+# print(type(x))
+# print(type(y))
+# z=x/y
+# print(type(z)) 
+# output-<class 'float'>
+# .........................................................................
+
+# Float
+
+# x=10.5
+# y=20.5
+# print(type(x))
+# print(type(y))
+# z=x+y
+# print(type(z))
+
+# x=10.5
+# y=20.5
+# print(type(x))
+# print(type(y))
+# z=x-y
+# print(type(z))
+
+# x=10.5
+# y=20.5
+# print(type(x))
+# print(type(y))
+# z=x*y
+# print(type(z))
+
+# x=10.5
+# y=20.5
+# print(type(x))
+# print(type(y))
+# z=x/y
+# print(type(z))
+
+# output-<class 'float'>
+# ------------------------------------------------------------------------------------------
+
+# complex
+
+# x=10.5+3j
+# y=20.5+5j
+# print(type(x))
+# print(type(y))
+# z=x/y
+# print(type(z))
+# output-<class 'complex'>
+# -------------------------------------------------------------------------------------------------
+
+# 
